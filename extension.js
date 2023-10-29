b562dc828a52d244b49ff82bac073f9c474d9bd9
Stephen Dias <42007943+STEPHENDIAS10@users.noreply.github.com>
2023-10-28 23:06:38 -0400

    5

--- extension.js
+++ extension.js
@@ -1,6 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const axios = require('axios');
+const xmlParser = require("fast-xml-parser")
+const https = require("https")
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -8,7 +12,30 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
+
+	const res  = await axios.get("https://blog.webdevsimplified.com/rss.xml")
+
+	const articles = xmlParser.parse(res.data).rss.channel.item.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate)).map(article => {
+      return {
+        label: article.title,
+        detail: article.description,
+        link: article.link,
+      }
+    })
+	
+	let disposable = vscode.commands.registerCommand(
+		"test-step-dias.search",
+		async function () {
+		  const article = await vscode.window.showQuickPick(articles, {
+			matchOnDetail: true,
+		  })
+	
+		  if (article == null) return
+	
+		  vscode.env.openExternal(article.link)
+		}
+	  )
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -17,12 +44,6 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('test-step-dias.search', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from test_step_dias!##############################');
-	});
 
 	context.subscriptions.push(disposable);
 }
